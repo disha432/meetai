14fb7c5c159c979bc93ce3118b44f7fb04fc7a26
Disha Wankhade <disha.wankhade06.com>
2025-12-20 18:48:17 +0530

    03 Authentication setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
-import { Button } from "@/components/ui/button";
+"use client"; // this is a client component // this is required for using components that have interactivity
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client"; //import the auth client
+import React from "react";
 
 export default function Home() {
+  const[name, setName] = React.useState("");
+  const[email, setEmail] = React.useState("");
+  const[password, setPassword] = React.useState("");
+
+  const onSubmit = () =>{
+     authClient.signUp.email({
+      name,
+      email,
+      password
+     },{
+      onError:()=>{
+        alert("Error signing up");
+      },
+      onSuccess:()=>{
+        alert("Signed up successfully");
+      }
+     });
+  }
+
   return(
-    <Button variant="ghost" size="lg">
-      Click Here
-    </Button>
+    <div className="flex flex-col gap-4 p-4">
+     <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+     <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+     <Input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    
+     <Button onClick={onSubmit}>Click Here</Button>
+    
+    </div>
+
   )
 }
